34e994009137f95fcd1a10f90d3eece3af1cbad6
Tabetha Walker-Smith <walkertasmith@gmail.com>
2017-07-17 12:17:55 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+//###################################################################
+
+// Dog Constructor & Prototype
+function Dog (name) { "hungry", "color", ""
+  this.name = name;
+  this.pets =
+  this.hungry =
+  this.color = black;
+
+}
+
+const sadie = new Dog("sadie","black",);
+const moonshine = new Dog("moonshine");
+const atticus = new Dog("atticus");
+
+
+
+
+function sadie() {
+  this.pet = "it should make sadie happy when Mason pets her";
+}
+
+
+function sadie() {
+  this.color = "it should make sadie black";
+
+}
+
+function moonshine()
+  this.hungry = "it should make moonshine hungry"
+
+// Instances ofs Dog
+// Needed: sadie, moonshine, atticus
+
+// Human Constructor & Prototype
+function Human (name) {
+  this.name = name;
+}
+const mason = new Human("mason");
+const julia = new Human("julia");
+// Instances of Human
+// Needed: mason, julia
