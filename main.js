4c9a395655888d236395c4e64e34165fda8f8000
Tabetha Walker-Smith <walkertasmith@gmail.com>
2017-07-17 13:11:48 -0700

    second commit

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
 //###################################################################
 
 // Dog Constructor & Prototype
-function Dog (name) { "hungry", "color", ""
-  this.name = name;
-  this.pets =
-  this.hungry =
-  this.color = black;
+function Dog(hungry) {
 
+  this.status = "normal";
+  this.color = "black";
+  this.hungry = "hungry";
 }
+//human
 
-const sadie = new Dog("sadie","black",);
-const moonshine = new Dog("moonshine");
-const atticus = new Dog("atticus");
-
-
-
-
-function sadie() {
-  this.pet = "it should make sadie happy when Mason pets her";
-}
+const sadie = new Dog(false);
+const moonshine = new Dog(true);
+const atticus = new Dog();
 
 
-function sadie() {
-  this.color = "it should make sadie black";
 
-}
+function Human(cool) {
+  this.cool = cool;
+  this.pet = function(dog) {
+      dog.status = "happy";
+  }
 
-function moonshine()
-  this.hungry = "it should make moonshine hungry"
 
-// Instances ofs Dog
-// Needed: sadie, moonshine, atticus
+  this.feed = function(dog) {
+    dog.hungry = false;
+  }
 
-// Human Constructor & Prototype
-function Human (name) {
-  this.name = name;
-}
-const mason = new Human("mason");
-const julia = new Human("julia");
-// Instances of Human
-// Needed: mason, julia
+
+  Human.prototype.pet = function(dog) {
+    dog.status = "happy";
+  }
+
+  Human.prototype.feed = function(dog) {
+    dog.hungry = false;
+  }}
+
+  const mason = new Human(true);
+  const julia = new Human(false);
+
+  // Instances of Human
+  // Needed: mason, julia
